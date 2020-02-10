1982a1df5c9642e14b46a3606ec1cd045ad267aa
amilicweb <aleksandar.milic035@gmail.com>
2020-02-11 00:49:38 +0100

    Added intro and navigation to website.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// $(window).scroll(function() {
+//   var scrollTop = $(window).scrollTop();
+
+//   if (scrollTop > 0) {
+//     $('#navbar').addClass('js-nav-scroll');
+//   } else {
+//     $('#navbar').removeClass('js-nav-scroll');
+//   }
+// });
